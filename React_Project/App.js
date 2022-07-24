40f8b9293a19db8fa05b3c30b3fb312ef13a889a
Lendus Casey <lendus.casey@gmail.com>
2022-07-24 16:13:36 -0500

    my first react native project

---
+++ React_Project/App.js
@@ -0,0 +1,48 @@
+import { StyleSheet, View, Button, TextInput, FlatList } from "react-native";
+
+import { useState } from "react";
+
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+
+export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, key: Math.random().toString() },
+    ]);
+  }
+
+  return (
+    <View style={styles.appContainer}>
+      <GoalInput onPress={addGoalHandler} onChangeText={goalInputHandler} />
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return <GoalItem text={itemData.item.text} />;
+          }}
+          alwaysBounceVertical={false}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+});
