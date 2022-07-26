1d659e169fbc456339cdc0def5c9ab8e9f6350d0
Lendus Casey <lendus.casey@gmail.com>
2022-07-25 22:31:01 -0500

    guessing game

---
+++ GuessingGame/screens/GameOverScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from 'react-native';
+
+const GameOverScreen = () => {
+    return(
+        <View>
+            <TextInput />
+            {/* Buttons */}
+        </View>
+    )
+};
+
+export default GameOverScreen;
